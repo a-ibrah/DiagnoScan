52f25aa88bd8d2b04e7e516905413dc9c0ff24ab
JigglyJello73 <anameunknowni@gmail.com>
2024-12-28 17:17:57 -0500

    Coin Toss

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "Hello, World!"
+def index():
+    # Render the front-end template
+    return render_template('index.html')
+
+@app.route('/flip', methods=['GET'])
+def flip_coin():
+    # Return "heads" or "tails" randomly
+    result = random.choice(["heads", "tails"])
+    return jsonify({"result": result})  # Return the result as JSON
 
 if __name__ == '__main__':
     app.run(debug=True)
